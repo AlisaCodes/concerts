f4790d29547466d93a1a0f42b3f2dffae7656161
alisacodes <alisaclone@gmail.com>
2015-08-28 11:50:04 -0700

    Add add bands and delete bands

--- app.rb
+++ app.rb
@@ -5,6 +5,25 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
-  "GET WITH IT!"
+  @bands = Band.all()
   erb(:index)
+end
+
+post('/bands') do
+
+  name = params.fetch('name')
+  Band.create({:name => name})
+  redirect back
+end
+
+delete('/bands/delete/:id') do
+  band = Band.find(params.fetch('id').to_i())
+  band.destroy
+  redirect back
+end
+
+get('/bands/:id') do
+  @band = Band.find(params.fetch('id').to_i)
+  @Venue = Venue.all()
+  erb(:band)
 end
